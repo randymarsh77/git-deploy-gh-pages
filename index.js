461db227b9a5838f4d23a2e9abceb5acbe749c44
randymarsh77 <mattpsbs@gmail.com>
2025-04-22 13:12:56 -0700

    Redo Windows incompatibility fix.

--- index.js
+++ index.js
@@ -88,8 +88,10 @@ const destinationPath = path.join(workingDirectory, relativeDestinationPath);
 
 shell.rm('-rf', branch);
 exec(`git clone -b ${branch} ${remote} ${branch}`);
-shell.rm('-rf', `${destinationPath}${path.sep}`);
-exec(`mkdir -p ${destinationPath}`);
+shell.rm('-rf', `${destinationPath}${path.sep}*`);
+if (!fs.existsSync(destinationPath)) {
+	exec(`mkdir -p ${destinationPath}`);
+}
 exec(`cp -r ${outDirectory}${path.sep}. ${destinationPath}${path.sep}.`);
 shell.rm('-rf', outDirectory);
 
